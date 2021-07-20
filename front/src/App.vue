2637ecbc475cad271612ff0669cef0df80d2201b
daiki-nishimura <daikiahe0@gmail.com>
2021-07-20 22:16:55 +0900

    feat: webpack 環境構築

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
